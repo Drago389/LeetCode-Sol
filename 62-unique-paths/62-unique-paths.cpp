cfb83d9b28d07dcc722dac9bc8f9a4fd8d18721d
Abhay Tripathi <abhaytripathi070@gmail.com>
2022-08-09 14:18:44 +0530

    Time: 2 ms (50.21%), Space: 6.5 MB (46.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    // MEMOIZATION CODE
     
     int f(int i,int j,vector<vector<int>>&dp){
         if(i==0 && j==0) return 1;
@@ -20,3 +21,30 @@ public:
         
     }
 };
+
+
+/* TABULATION CODE 
+
+int uniquePaths(int m,int n){
+    int dp[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 && j==0) dp[i][j]=1;
+            else{
+                    int up=0;
+                    int left =0;
+                    if(i>0) up= dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]= up + left;
+                
+                }
+        }
+    }
+    return dp[m-1][n-1];
+}
+
+
+
+
+
+*/
