19836ac0b58981fd4189d263e7c1d68b09d89bf2
Abhay Tripathi <abhaytripathi070@gmail.com>
2022-08-09 15:59:24 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (64.25%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
     
+    // MEMOIZATION
 
 
     int f(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>&dp){
@@ -26,3 +27,32 @@ public:
         return f(m-1,n-1,obstacleGrid,dp);
     }
 };
+
+
+
+/* TABULATION 
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j==1]) return 0;
+                    else if(i==0 && j==0) dp[i][j]=0;
+                        else
+                        {
+                        int up=0,left=0;
+                    if(i>0) up= dp[i-1][j];
+                     if(j>0) left=dp[i][j-1];
+                     
+                     dp[i][j] = left + up;
+                        
+                        }
+                
+            }
+        }
+        return dp[m-1][n-1];
+
+*/
