2e9275e1a91083ac381c548a020da8091a59fd27
Abhay Tripathi <abhaytripathi070@gmail.com>
2022-08-09 16:48:54 +0530

    Time: 17 ms (42.94%), Space: 10.1 MB (29.24%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    //MEMOIZATION 
     int f(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&dp){
         if(i==0 && j==0) return grid[i][j];
         if(i<0 || j<0) return 1e9;
@@ -21,6 +22,43 @@ public:
         vector<vector<int>>dp(m,vector<int>(n,-1));
         
         return f(m-1,n-1,grid,dp);
+        
+       /* 
+       TABULATION
+       
+       vector<vector<int>>dp(m,vector<int>(n,0));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0; j<n;j++){
+                if(i==0 && j==0) return dp[i][j]= grid[i][j];
+                else{
+                    int up= grid[i][j];
+                    if(i>0) up+= dp[i-1][j];
+                    else up+= 1e9;
+                    
+                
+                    int left =grid[i][j];
+                    if(j>0) left+= dp[i][j-1];
+                    else left+= 1e9;
+                
+                    
+                    dp[i][j]= min(up,left);
+                    
+                }
+            }
+        }
+        return dp[m-1][n-1];
+        
+       */ 
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 };
