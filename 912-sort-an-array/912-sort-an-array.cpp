a2a40f575ae3cedaec474b69194c54f8c2304474
Abhay Tripathi <abhaytripathi070@gmail.com>
2022-08-11 00:36:54 +0530

    Time: 93 ms (63.94%), Space: 31.1 MB (32.57%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int,vector<int>,greater<int>> minHeap;
+        
+        for(int i=0;i<nums.size();i++){
+            minHeap.push(nums[i]);
+            
+        }
+        vector<int>ans;
+        while(!minHeap.empty()){
+            int t= minHeap.top();
+            minHeap.pop();
+            ans.push_back(t);
+            
+        }
+        return ans;
+        
+    }
+
+};
